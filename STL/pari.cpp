9ed4e2040f61bcf79816d5243652ea77fd5ab4e7
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-24 09:23:37 +0500

    pair in STL

---
+++ STL/pari.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // pair contains the pair of two hetrogenous elements like this
+    // pair(first, second ) pariName;
+
+    pair<string, int> abdullah = {"Abdullah", 20};
+    cout << abdullah.first << endl;
+    cout << abdullah.second << endl;
+    return 0;
+}
