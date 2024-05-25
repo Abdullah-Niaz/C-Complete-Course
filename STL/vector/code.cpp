1b89b0eb8d5df8aa2370c8ff3967c9f379aaef70
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-25 23:13:09 +0500

    vector

--- STL/vector/code.cpp
+++ STL/vector/code.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 int main(){
-    pair<string,int> pair_name = {"Abdullah",20};
-    pair_name.first  = "Omer";
-    pair_name.second  = 20;
-    cout<<pair_name.first<<endl;   
-    cout<<pair_name.second<<endl;   
+    // pair<string,int> pair_name = {"Abdullah",20};
+    // pair_name.first  = "Omer";
+    // pair_name.second  = 20;
+    // cout<<pair_name.first<<endl;   
+    // cout<<pair_name.second<<endl; 
+
+    vector<int> v;
+    v.push_back(1);  
+    v.push_back(2);  
+    v.push_back(3);  
+    v.push_back(4);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+      
     
     return 0;
 }
