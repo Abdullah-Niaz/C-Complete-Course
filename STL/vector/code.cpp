7ae9931aae5bff8ddae7652cce89e356e723fa45
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-25 23:18:45 +0500

    iterators over vectors

--- STL/vector/code.cpp
+++ STL/vector/code.cpp
@@ -1,28 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     // pair<string,int> pair_name = {"Abdullah",20};
     // pair_name.first  = "Omer";
     // pair_name.second  = 20;
-    // cout<<pair_name.first<<endl;   
-    // cout<<pair_name.second<<endl; 
+    // cout<<pair_name.first<<endl;
+    // cout<<pair_name.second<<endl;
 
     vector<int> v;
-    v.push_back(1);  
-    v.push_back(2);  
-    v.push_back(3);  
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
     v.push_back(4);
 
     v.emplace_back(5);
     v.emplace_back(6);
     v.emplace_back(7);
-    
-    for (int i = 0; i < v.size(); i++)
+
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+
+    vector<int>::iterator it = v.begin();
+    for (; it != v.end(); ++it)
     {
-        cout<<v[i]<<" ";
+        cout << *(it) << " ";
     }
-      
-    
+
+    cout<<endl;
+
+    vector<int>::iterator its = v.end();
+    its--;
+    cout << "Its Value: " << *(its) << endl;
     return 0;
 }
