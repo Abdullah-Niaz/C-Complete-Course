ab9f425dbaa6066d308643ea9f206fa238a11e18
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-28 23:59:53 +0500

    erase Fucntion

--- STL/vector/vector_func.cpp
+++ STL/vector/vector_func.cpp
@@ -2,12 +2,42 @@
 using namespace std;
 
 int main(){
-    vector<int> v(5,100);
-    for (auto it = v.begin() ; it != v.end() ; it++)
-    {
-        cout<<*(it) <<" ";
-    }
-      
+    vector<int> v  = {1,2,3,4,5};
+    // for (auto it = v.begin() ; it != v.end() ; it++)
+    // {
+    //     cout<<*(it) <<" ";
+    // }
+    // cout<<endl;
+
+    // vector<int>::reverse_iterator its = v.rbegin();
+    // cout<<*(its);
+    // cout<<endl;
+
+    // cout<<v[0] << " " <<endl;
+
+    // cout<<v.back();
+    
+    // Erase Functions 
+    // v.erase(v.begin());
+    //  for (auto it = v.begin() ; it != v.end() ; it++)
+    // {
+    //     cout<<*(it) <<" ";
+    // }
+    // cout<<endl;
+    // v.erase(v.begin() + 1 );
+
+    //  for (auto it = v.begin() ; it != v.end() ; it++)
+    // {
+    //     cout<<*(it) <<" ";
+    // }
+    // cout<<endl;
+    // v.erase(v.begin() , v.end() );
+    //  for (auto it = v.begin() ; it != v.end() ; it++)
+    // {
+    //     cout<<*(it) <<" ";
+    // }
+    // cout<<endl;
+ 
     
     return 0;
 }
