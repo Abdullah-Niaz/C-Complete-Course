cf1b195f0829925dbea2cbcbeeed1236dd42079d
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-28 23:34:51 +0500

    vector

--- STL/vector/code.cpp
+++ STL/vector/code.cpp
@@ -9,31 +9,36 @@ int main()
     // cout<<pair_name.first<<endl;
     // cout<<pair_name.second<<endl;
 
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
+    // vector<int> v;
+    // v.push_back(1);
+    // v.push_back(2);
+    // v.push_back(3);
+    // v.push_back(4);
 
-    v.emplace_back(5);
-    v.emplace_back(6);
-    v.emplace_back(7);
+    // v.emplace_back(5);
+    // v.emplace_back(6);
+    // v.emplace_back(7);
 
     // for (int i = 0; i < v.size(); i++)
     // {
     //     cout<<v[i]<<" ";
     // }
 
-    vector<int>::iterator it = v.begin();
-    for (; it != v.end(); ++it)
-    {
-        cout << *(it) << " ";
+    // vector<int>::iterator it = v.begin();
+    // for (; it != v.end(); ++it)
+    // {
+    //     cout << *(it) << " ";
+    // }
+
+    // cout<<endl;
+
+    // vector<int>::iterator its = v.end();
+    // its--;
+    // cout << "Its Value: " << *(its) << endl;
+
+    vector<int> v(5,100);
+    for(int i = 0 ; i < v.size();  i++){
+        cout<<v[i]<<" ";
     }
-
-    cout<<endl;
-
-    vector<int>::iterator its = v.end();
-    its--;
-    cout << "Its Value: " << *(its) << endl;
     return 0;
 }
