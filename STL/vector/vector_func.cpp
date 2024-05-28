ed43b9e181347b3986e6fd58d21bfc6dbb8bea53
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-28 23:43:17 +0500

    vector Iterations

---
+++ STL/vector/vector_func.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v(5,100);
+    for (auto it = v.begin() ; it != v.end() ; it++)
+    {
+        cout<<*(it) <<" ";
+    }
+      
+    
+    return 0;
+}
