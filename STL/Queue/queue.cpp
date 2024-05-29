0a90fefbdb0dc04e8a7d5d5eab0fbd008ef32c22
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-29 14:57:52 +0500

    Queue

---
+++ STL/Queue/queue.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    queue<int> v;
+    v.push(1);   
+    v.push(2);   
+    v.push(3);   
+    v.push(4);
+
+    cout<<v.back() <<endl;
+    cout<<v.front() <<endl;
+    
+      
+    
+    return 0;
+}
